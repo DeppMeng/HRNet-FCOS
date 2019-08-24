3c7868213525d5c071f0ee4218c8fdf98bb02697
Depu Meng (FA Talent) <v-depme@microsoft.com>
2019-08-24 18:26:43 +0800

    debugging in evaluation by json file

--- maskrcnn_benchmark/data/datasets/evaluation/coco/coco_eval.py
+++ maskrcnn_benchmark/data/datasets/evaluation/coco/coco_eval.py
@@ -379,6 +379,7 @@ def evaluate_predictions_on_coco(
     from pycocotools.cocoeval import COCOeval
 
     coco_dt = coco_gt.loadRes(str(json_result_file)) if coco_results else COCO()
+    print(coco_dt[0])
 
     # coco_dt = coco_gt.loadRes(coco_results)
     coco_eval = COCOeval(coco_gt, coco_dt, iou_type)
