c20a800f24cbfc13bf945988da58a95ab6cae764
Depu Meng (FA Talent) <v-depme@microsoft.com>
2019-08-24 23:26:44 +0800

    debugging in evaluation by json file

--- maskrcnn_benchmark/data/datasets/evaluation/coco/coco_eval.py
+++ maskrcnn_benchmark/data/datasets/evaluation/coco/coco_eval.py
@@ -404,6 +404,8 @@ def evaluate_predictions_only_human_on_coco(
 
     coco_dt = coco_gt.loadRes(str(json_result_file)) if coco_results else COCO()
 
+    print(coco_dt.dataset['annotations'][0])
+    print(coco_gt.dataset['annotations'][0])
     # coco_dt = coco_gt.loadRes(coco_results)
     coco_eval = COCOeval(coco_gt, coco_dt, iou_type)
     coco_eval.params.catIds = [1]
